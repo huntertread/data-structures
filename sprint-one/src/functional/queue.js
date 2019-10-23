9573951c85bf5974f280f97eb5d8cbf4c2edcbae
Hunter Treadaway <huntertreadaway@Hunters-MacBook-Pro.local>
2019-10-23 15:31:30 -0700

    completed the functional pattern problems (stack and queue)

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,36 @@
 var Queue = function() {
   var someInstance = {};
-
   // Use an object with numeric keys to store values
-  var storage = {};
-
+  someInstance.storage = {};
+  var position = 0;
+  var size = 0;
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    someInstance.storage[position] = value;
+    position++;
+    size++;
   };
 
   someInstance.dequeue = function() {
+    var currentPosition = position;
+    for (key in someInstance.storage) {
+      if (key < currentPosition) {
+        currentPosition = key;
+      }
+    }
+    var dequeue = someInstance.storage[currentPosition];
+    delete someInstance.storage[currentPosition];
+    size--;
+    return dequeue;
   };
 
   someInstance.size = function() {
+    if (size < 0) {
+      size = 0;
+      return size;
+    } else {
+      return size;
+    }
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,17 +2,47 @@ var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  someInstance.storage = {};
+  var position = 1;
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    someInstance.storage[position] = value;
+    position++;
+    size++;
   };
 
   someInstance.pop = function() {
+    var poppedValue = someInstance.storage[position - 1];
+    delete someInstance.storage[position - 1];
+    position--;
+    size--;
+    return poppedValue;
   };
 
   someInstance.size = function() {
+    if (size < 0) {
+      size = 0;
+      return size;
+    } else {
+      return size;
+    }
   };
 
   return someInstance;
 };
+
+// stack = new Stack;
+// stack.push('a');
+// console.log(stack.storage);
+// console.log(stack.pop());
+// stack.pop();
+// console.log(stack.storage);
+// stack.push('b');
+// stack.push('c');
+// console.log(stack.storage);
+// console.log(stack.pop());
+// stack.pop();
+// console.log(stack.storage);
+
