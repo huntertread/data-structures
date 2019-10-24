32e0b705df798d1a827bcde5fdb737e5d4a21dca
Hunter Treadaway <huntertreadaway@Hunters-MacBook-Pro.local>
2019-10-24 14:16:03 -0700

    completed functional-shared stack problem

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,55 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  newStack.storage = {};
+  newStack.sizeVal = 0;
+  newStack.position = 0;
+  newStack.push = push;
+  newStack.pop = pop;
+  newStack.size = size;
+  return newStack;
 };
 
-var stackMethods = {};
+var push = function(value) {
+  var position = this.position;
+  var sizeVal = this.sizeVal;
+  this.storage[position] = value;
+  this.position++;
+  this.sizeVal++;
+};
 
+var pop = function() {
+  var position = this.position;
+  var sizeVal = this.sizeVal;
+  var poppedVal = this.storage[position - 1];
+  delete this.storage[position - 1];
+  this.position--;
+  this.sizeVal--;
+  return poppedVal;
+};
 
+var size = function() {
+  var sizeVal = this.sizeVal;
+  if (sizeVal < 0) {
+    sizeVal = 0;
+    return sizeVal;
+  } else {
+    return sizeVal;
+  }
+};
+
+// stack = new Stack;
+// stack.push('a');
+// console.log(stack.storage);
+// console.log(stack.sizeVal);
+// console.log(stack.pop());
+// stack.pop();
+// console.log(stack.storage);
+// stack.push('b');
+// stack.push('c');
+// console.log(stack.storage);
+// stack.push('d');
+// console.log(stack.storage);
+// console.log(stack.pop());
+// stack.pop();
+// console.log(stack.storage);
+// console.log(stack);
