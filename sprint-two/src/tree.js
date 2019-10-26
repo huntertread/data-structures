2a19669130cfd722976db027c22f70060d84e2f1
Hunter Treadaway <huntertreadaway@Hunters-MacBook-Pro.local>
2019-10-25 20:31:16 -0700

    tree progress

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,23 +1,68 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  extend(newTree, treeMethods);
+  newTree.children = [];  // fix me <- we fixed? changed null to []
   return newTree;
 };
 
-var treeMethods = {};
+var treeMethods = {
 
-treeMethods.addChild = function(value) {
+  addChild: function(value) {
+    var child = Tree(value);
+    this.children.push(child);
+    return child;
+  },
+
+  contains: function(target, currentNode) {
+    // currentNode = root node || current node
+    var currentNode = currentNode || this;
+    var containsValue = false;
+    // if currentNode contains target
+    if (currentNode.value === target) {
+      // return true
+      containsValue = true;
+    // else if currentNode.children
+    } else if (currentNode.value !== target && currentNode.children.length) {
+      for (var i = 0; i < currentNode.children.length; i++) {
+        return currentNode.children[i].contains(target, currentNode.children[i]);
+      }
+      // // iterate over the children array
+      // currentNode.children.forEach(function(element) {
+      //   // recurse contains fn on each index
+      //   if (element.contains(target, element)) {
+      //     return true;
+      //   } else {
+      //     element.contains(target, element);
+      //   }
+      // });
+    // if value is never found
+    } else {
+      // return false
+      return containsValue;
+    }
+  }
 };
 
-treeMethods.contains = function(target) {
-};
+/* --- extend function --- */
 
+var extend = function (destination, source) {
+  for (var key in source) {
+    destination[key] = source[key];
+  }
+};
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+/* --- tests ---*/
+
+var tree1 = Tree(1); // original tree
+console.log(tree1);
+tree1.addChild(2); // original tree's child
+console.log(tree1.children);
+tree1.children[0].addChild(3); // childs child
+console.log(tree1.children[0]);
+console.log(tree1.contains(2));
