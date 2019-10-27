f9ca636525beeea7c3da4d8ac24cb61c2390ac87
Hunter Treadaway <huntertreadaway@Hunters-MacBook-Pro.local>
2019-10-27 16:06:47 -0700

    completed hash function

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,34 +5,54 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  // if value already at index
-  // push an array into the index
-  // set the existing value as the first index
-  // push the new value
-  // if there is already an array at index
-  // push the new value to the array
-
-  // insert v into _storage @ the index value
-  this._storage.set(index, v);
+  // if nothing @ index
+  if (this._storage.get(index) === undefined) {
+    // create a tupleBucket variable with value of [k, v]
+    var tupleBucket = [[k, v]];
+    // push key/value tuple in to bucket
+    this._storage.set(index, tupleBucket);
+  // if k already exists at index
+  } else if (this.retrieve(k)) {
+    // delete the current value
+    this.remove(k);
+    return this._storage.get(index).push([k, v]);
+  // if there is already a bucket array at index
+  } else if (this._storage.get(index) !== undefined) {
+    // push the new key/value tuple to the array
+    return this._storage.get(index).push([k, v]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  // use get to retrieve the value
-  return this._storage.get(index);
+  var bucket = this._storage.get(index);
+  // iterate over bucket @ index
+  for (var i = 0; i < bucket.length; i++) {
+    // if bucket[i] is defined
+    if (bucket[i] !== undefined) {
+      // check for k @ each index[0]
+      if (bucket[i][0] === k) {
+        // if match found return index[1]
+        return bucket[i][1];
+      }
+    }
+  }
 };
+
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var self = this;
-  this._storage.each(function(element, i) {
-    // if index is equal to i
-    if (index === i) {
-      // delete storage[i]
-      self._storage.set(i, undefined);
+  // iterate over index
+  for (var i = 0; i < this._storage.get(index).length; i++) {
+    // if index[i][0] equals k
+    if (this._storage.get(index)[i][0] === k) {
+      // delete index[i]
+      delete this._storage.get(index)[i];
     }
-  });
+  }
 };
+// console.log(this._storage.get(index)[0].includes(k), 'hi');
 
 /* --- tests --- */
 var testTable = new HashTable;
